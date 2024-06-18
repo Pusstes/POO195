17eb3393462aad90c1b4786d4c9ad4a543134b62
Gerardo Alberto Ramirez Mejorada <147885471+Pusstes@users.noreply.github.com>
2024-06-17 20:31:21 -0600

    Modificacion para la consulta

--- CrudAlbums/app.py
+++ CrudAlbums/app.py
@@ -1,34 +1,45 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost' #host de la base de datos
-app.config['MYSQL_USER'] = 'root' #usuario de la base de datos
-app.config['MYSQL_PASSWORD'] = '' #contraseña de la base de datos
-app.config['MYSQL_DB'] = 'bdflask' #nombre de la base de datos
-
-MySQL = MySQL(app) #se crea una instancia de MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'bdflask'
+mysql = MySQL(app)
+app.secret_key = 'mysecretkey'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM albums')
+        consultaA = cursor.fetchall()
+        return render_template('index.html', albums=consultaA)
+    except Exception as e:
+        print(e)
+        # Retorna una respuesta en caso de error
+        return render_template('error.html', error_message=str(e))
 
-@app.route('/guardarAlbum', methods=['POST']) #ruta para guardar un album
-def guardarAlbum(): #funcion para guardar un album
-    if request.method == 'POST': #validacion del metodo
-        titulo = request.form['titulo']
-        artista = request.form['artista']
-        anio = request.form['anio']
-        #valores que se obtienen del formulario
-        print(titulo, artista, anio)
-        return 'Recibido'   
+@app.route('/guardarAlbum', methods=['POST'])
+def guardarAlbum():
+    if request.method == 'POST':
+        Ftitulo = request.form['titulo']
+        Fartista = request.form['artista']
+        Fanio = request.form['anio']
 
-#menejo de excepciones para rutas no encontradas
+        # Enviamos a la BD
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO albums(titulo, artista, anio) VALUES(%s, %s, %s)', (Ftitulo, Fartista, Fanio))
+        mysql.connection.commit()
+        flash('Album Guardado Correctamente')
+        return redirect(url_for('index'))
+
+# Manejo de excepciones
 @app.errorhandler(404)
 def paginano(e):
-    return 'Revisa tu sintaxis: PAGINA NO ENCONTRADA'
-#en esta ruta se muestra un mensaje de error, se puede acceder a ella desde la URL http://localhost:3000/otra
+    return 'Revisa tu sintaxis: No encontré nada'
 
 if __name__ == '__main__':
-    #port es el puerto donde se ejecutará la aplicación y debug=True activa el modo de depuración.
-    app.run(port=4000, debug=True)
+    # port es el puerto donde se ejecutará la aplicación y debug=True activa el modo de depuración.
+    app.run(port=3000, debug=True)
