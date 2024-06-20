e2f4cdba3c32743503ab7b67a769a0884bc8633a
Gerardo Alberto Ramirez Mejorada <147885471+Pusstes@users.noreply.github.com>
2024-06-19 20:29:39 -0600

    Termine la practica en clase

--- PruebasFlask/app.py
+++ PruebasFlask/app.py
@@ -1,15 +1,15 @@
-from flask import Flask,request,render_template,url_for,redirect,flash
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost' #host de la base de datos
-app.config['MYSQL_USER'] = 'root' #usuario de la base de datos
-app.config['MYSQL_PASSWORD'] = '' #contraseña de la base de datos
-app.config['MYSQL_DB'] = 'bdflask' #nombre de la base de datos
+app.config['MYSQL_HOST'] = 'localhost'  # host de la base de datos
+app.config['MYSQL_USER'] = 'root'  # usuario de la base de datos
+app.config['MYSQL_PASSWORD'] = ''  # contraseña de la base de datos
+app.config['MYSQL_DB'] = 'hospital'  # nombre de la base de datos
 
 app.secret_key = 'my_secret'
 
-MySQL = MySQL(app) #se crea una instancia de MySQL
+mysql = MySQL(app)  # se crea una instancia de MySQL
 
 @app.route('/')
 def home():
@@ -18,6 +18,14 @@ def home():
 @app.route('/registro')
 def registro():
     return render_template('registro.html')
+
+@app.route('/consulta')
+def consulta():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM medicos')
+    medicos = cursor.fetchall()
+    cursor.close()
+    return render_template('Consulta.html', medicos=medicos)
 
 @app.route('/GuardarMedico', methods=['POST'])
 def guardarMedico():
@@ -28,18 +36,18 @@ def guardarMedico():
         fcorreo = request.form['correo']
         fpass = request.form['pass']
         frol = request.form['rol']
-        cursor = MySQL.connection.cursor()
-        cursor.execute('INSERT INTO t_medicos (rfc, nombre, cedula, correo, pass, rol) VALUES (%s, %s, %s, %s, %s, %s)', (frfc, fnombre, fcedula, fcorreo, fpass, frol))
-        flash('El medico fue registrado Correctamente')
-        return redirect(url_for('registro'))
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO medicos (rfc, nombre, cedula, correo, pass, rol) VALUES (%s, %s, %s, %s, %s, %s)', (frfc, fnombre, fcedula, fcorreo, fpass, frol))
+        mysql.connection.commit()
+        cursor.close()
+        flash('El médico fue registrado correctamente')
+        return redirect(url_for('consulta'))
 
-#menejo de excepciones para rutas no encontradas
+# Manejo de excepciones para rutas no encontradas
 @app.errorhandler(404)
 def paginano(e):
     return 'Revisa tu sintaxis: PAGINA NO ENCONTRADA'
-#en esta ruta se muestra un mensaje de error, se puede acceder a ella desde la URL http://localhost:3000/otra
 
 if __name__ == '__main__':
-    #port es el puerto donde se ejecutará la aplicación y debug=True activa el modo de depuración.
+    # puerto donde se ejecutará la aplicación y debug=True activa el modo de depuración.
     app.run(port=4000, debug=True)
-    
